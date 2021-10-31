a2d14fd89f13fa84d82d69bdc2844073ea73125e
河合彰紘 <rakko1124@gmail.com>
2021-10-31 19:16:55 +0900

    Initial Commit

---
+++ LandMarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LandMarks
+//
+//  Created by 河合彰紘 on 2021/10/31.
+//  Copyright © 2021 河合彰紘. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
